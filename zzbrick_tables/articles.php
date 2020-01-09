01e03bee27ff819b78152ece2a14aaf696f1cb2b
Gustaf Mossakowski <gustaf@koenige.org>
2020-01-09 17:53:45 +0000

    allow up to 40 events per article
    git-svn-id: file:///Users/Shared/Repos/modules/news/trunk@41 eaf79678-5e9d-4c39-9089-411efe802c85

--- zzbrick_tables/articles.php
+++ zzbrick_tables/articles.php
@@ -115,6 +115,7 @@ $zz['fields'][22] = zzform_include_table('articles-events');
 $zz['fields'][22]['title'] = 'Events';
 $zz['fields'][22]['type'] = 'subtable';
 $zz['fields'][22]['min_records'] = 0;
+$zz['fields'][22]['max_records'] = 40;
 $zz['fields'][22]['hide_in_list'] = true;
 $zz['fields'][22]['form_display'] = 'lines';
 $zz['fields'][22]['sql'] .= ' ORDER BY /*_PREFIX_*/articles.date DESC, sequence';
