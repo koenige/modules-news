91d3dfdfe70182e51ec036b24beb2999d8eba59c
Gustaf Mossakowski <gustaf@koenige.org>
2020-12-01 13:23:03 +0000

    rename data script for articles to articledata
    git-svn-id: file:///Users/Shared/Repos/modules/news/trunk@97 eaf79678-5e9d-4c39-9089-411efe802c85

--- zzbrick_request_get/articledata.inc.php
+++ zzbrick_request_get/articledata.inc.php
@@ -23,7 +23,7 @@
  * @param string $lang_field_name (optional, if not current language shall be used)
  * @return array
  */
-function mod_news_get_articles($data, $settings = [], $id_field_name = '', $lang_field_name = '') {
+function mod_news_get_articledata($data, $settings = [], $id_field_name = '', $lang_field_name = '') {
 	if (!$data) return $data;
 	global $zz_setting;
 	require_once $zz_setting['core'].'/data.inc.php';
