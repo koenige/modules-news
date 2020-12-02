0584663b2d66c0d1a77695394708d3f29da90e5e
Gustaf Mossakowski <gustaf@koenige.org>
2020-12-02 10:02:07 +0000

    add request/get function for articles, basic functionality
    git-svn-id: file:///Users/Shared/Repos/modules/news/trunk@101 eaf79678-5e9d-4c39-9089-411efe802c85

---
+++ zzbrick_request_get/articles.inc.php
@@ -0,0 +1,66 @@
+<?php 
+
+/**
+ * news module
+ * get article data
+ *
+ * Part of »Zugzwang Project«
+ * http://www.zugzwang.org/modules/news
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2020 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+/**
+ * get article data
+ *
+ * @param array $params
+ * @param array $settings
+ * @return array
+ */
+function mod_news_get_articles($params, $settings = []) {
+	global $zz_setting;
+	if (count($params) > 1) return false;
+
+	$news_category_id = wrap_category_id('news');
+
+	$where = [];
+	$limit = '';
+	if ($zz_setting['local_access']) {
+		$where[] = '(articles.published = "yes" OR articles.published = "no")';
+	} else {
+		$where[] = 'articles.published = "yes"';
+	}
+
+	// Articles
+	$sql = 'SELECT articles.article_id
+		FROM articles
+		LEFT JOIN articles_categories
+			ON articles.article_id = articles_categories.article_id
+			AND articles_categories.type_category_id = %d
+		LEFT JOIN categories USING (category_id)
+		WHERE (ISNULL(date_to) OR date_to >= CURDATE())
+		%s
+		ORDER BY date DESC, time DESC, identifier DESC
+		%s
+	';
+	$sql = sprintf($sql
+		, $news_category_id
+		, ($where ? ' AND '.implode(' AND ', $where) : '')
+		, $limit
+	);
+	$ids = wrap_db_fetch($sql, 'article_id');
+
+	require_once $zz_setting['modules_dir'].'/news/zzbrick_request_get/articledata.inc.php';
+	$articles = mod_news_get_articledata($ids);
+
+	$i = 0;
+	foreach (array_keys($articles) as $id) {
+		$i++;
+		$articles[$id]['article_no'] = $i;
+	}
+
+	return $articles;
+}
