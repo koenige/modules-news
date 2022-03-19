9334bbd8740befd59c0e09da6d42025ac71e0e43
Gustaf Mossakowski <gustaf@koenige.org>
2022-03-19 14:01:48 +0000

    articles/get: allow more than one parameter (e. g. publication + year); support setting 'months' for last n months; support parameters: optional news category/publication category + year + month; support setting 'hide_no_archive' to hide articles with category parameter 'no_archive'
    git-svn-id: file:///Users/Shared/Repos/modules/news/trunk@168 eaf79678-5e9d-4c39-9089-411efe802c85

--- zzbrick_request_get/articles.inc.php
+++ zzbrick_request_get/articles.inc.php
@@ -22,27 +22,65 @@
  */
 function mod_news_get_articles($params = [], $settings = []) {
 	global $zz_setting;
-	if (count($params) > 1) return false;
 
-	$news_category_id = wrap_category_id('news', 'check') ? wrap_category_id('news') : false;
-	$limit = '';
+	// news categories
+	$news_categories = ['publications', 'news'];
+	$news_categories_ids = [];
+	foreach ($news_categories as $index => $path) {
+		if (!$category_id = wrap_category_id($path, 'check')) {
+			unset($news_categories[$index]);
+			continue;
+		}
+		$news_categories_ids[] = $category_id; 
+	}
 
+	// conditions
 	$where = [];
-	if (!empty($_SESSION['logged_in'])) {
-		$where[] = '(articles.published = "yes" OR articles.published = "no")';
-	} else {
+	$join = [];
+	if (!empty($settings['months'])) {
+		$where[] = sprintf('DATEDIFF(CURDATE(), date) < %d * 30', $settings['months']);
+	}
+	if (empty($_SESSION['logged_in'])) {
 		$where[] = 'articles.published = "yes"';
 		$where[] = 'date <= CURDATE() AND (ISNULL(date_to) OR date_to >= CURDATE())';
 	}
-	
-	if ($news_category_id) {
-		$join = sprintf(' LEFT JOIN articles_categories
-			ON articles.article_id = articles_categories.article_id
-			AND articles_categories.type_category_id = %d
-			LEFT JOIN categories USING (category_id)
-		', $news_category_id);
-	} else {
-		$join = '';
+	if ($params) {
+		$param = array_shift($params);
+		
+		// check: is it a category?
+		foreach ($news_categories as $index => $path) {
+			if (!$category_id = wrap_category_id(sprintf('%s/%s', $path, $param), 'check')) continue;
+			$join[] = sprintf(' LEFT JOIN articles_categories
+				ON articles.article_id = articles_categories.article_id
+				AND articles_categories.type_category_id = %d
+				LEFT JOIN categories USING (category_id)
+			', wrap_category_id($path));
+			$where[] = sprintf('articles_categories.category_id = %d', $category_id);
+			$param = array_shift($params); // allow another parameter
+			break;
+		}
+		if (is_numeric($param)) {
+			$where[] = sprintf('YEAR(date) = %d', $param);
+			$param = array_shift($params); // allow another parameter
+		}
+		if (is_numeric($param)) {
+			$where[] = sprintf('MONTH(date) = %d', $param);
+			$param = array_shift($params); // allow another parameter
+		}
+		if ($params) return false; // wrong parameter count, illegal parameters
+	}
+	if (!empty($settings['hide_no_archive']) AND $news_categories) {
+		// hide_no_archive can be set per news category or publication
+		$sql = 'SELECT article_id
+			FROM articles
+			JOIN articles_categories USING (article_id)
+			JOIN categories USING (category_id)
+			WHERE main_category_id IN (%s)
+			AND parameters LIKE "%%&no_archive=1%%"';
+		$sql = sprintf($sql, implode(',', $news_categories_ids));
+		$hidden_article_ids = wrap_db_fetch($sql, 'article_id', 'single value');
+		if ($hidden_article_ids)
+			$where[] = sprintf('articles.article_id NOT IN (%s)', implode(',', $hidden_article_ids));
 	}
 
 	// Articles
@@ -54,9 +92,9 @@ function mod_news_get_articles($params = [], $settings = []) {
 		%s
 	';
 	$sql = sprintf($sql
-		, $join
+		, implode("\n", $join)
 		, $where ? 'WHERE '.implode(' AND ', $where) : ''
-		, $limit
+		, !empty($settings['last']) ? sprintf('LIMIT 0, %d', $settings['last']) : ''
 	);
 	$ids = wrap_db_fetch($sql, 'article_id');
 
