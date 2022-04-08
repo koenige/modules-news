70d457f02ff3a7b964aadaf8909f8772ee601206
Gustaf Mossakowski <gustaf@koenige.org>
2022-04-08 17:52:53 +0000

    articles: use 'titles' from get function; use 'count' from get function; simplify code, add support for publications (no need to set type=publications anymore)
    git-svn-id: file:///Users/Shared/Repos/modules/news/trunk@171 eaf79678-5e9d-4c39-9089-411efe802c85

--- zzbrick_request/articles.inc.php
+++ zzbrick_request/articles.inc.php
@@ -24,61 +24,51 @@ function mod_news_articles($params, $settings) {
 		if (!$data and !$events) $events = mf_events_in_news('latest');
 		$data = mf_events_in_news_sort($data, $events);
 	}
-
-	$title_prefix = wrap_text('News Articles');
-	if (!empty($settings['title_prefix'])) $title_prefix = $settings['title_prefix'];
-
-	if ($params AND is_numeric(end($params))) {
-		// overview year
-		if (empty($settings['hide_title'])) {
-			$page['title'] = $title_prefix.' '.end($params);
-		}
-		if (!$data) {
-			$data['no_news_year'] = true;
-			// events?
-			if (!empty(wrap_get_setting('news_with_events'))) {
-				$sql = 'SELECT COUNT(event_id) FROM events WHERE YEAR(date_begin) = %d';
-				$sql = sprintf($sql, end($params));
-				$events = wrap_db_fetch($sql);
-			} else {
-				$events = [];
-			}
-			if (!$events) {
-				$page['status'] = 404;
-			}
-		}
-		$page['breadcrumbs'][] = end($params);
-	} elseif ($params AND $params[0] !== 'homepage') {
-		// category overview
-		if (!$data) {
-			$data['no_news_category'] = true;
-			$page['status'] = 404;
-		}
-		$type = !empty($settings['type']) ? $settings['type'] : 'news';
-		$sql = 'SELECT category_id, category
-			FROM categories WHERE path = "%s/%s"';
-		$sql = sprintf($sql, wrap_get_setting($type.'_category_path'), wrap_db_escape($params[0]));
-		$category = wrap_db_fetch($sql);
-		if (!$category) {
-			$data['category_not_found'] = true;
-			$page['breadcrumbs'][] = wrap_text('Not Found');
-			$page['text'] = wrap_template('articles', $data);
-			return $page;
-		}
-		if (empty($settings['hide_title'])) {
-			$page['title'] = $title_prefix.' '.$category['category'];
-		}
-		$page['breadcrumbs'][] = $category['category'];
-	} else {
-		// individual view, e. g. homepage
-		if (empty($settings['hide_title'])) {
-			$page['title'] = wrap_text('News Articles');
-		}
-		if (!$data) {
-			$data['no_news_current'] = true;
-		}
+	if (!$data AND $params AND !is_numeric($params[0])) {
+		$data = [];
+		$data['category_not_found'] = true;
+		$data['titles'] = [];
+		$data['count'] = 0;
 	}
 	
+	if (empty($settings['hide_title'])) {
+		$page['title'][] = !empty($settings['title_prefix'])
+			? $settings['title_prefix'] : wrap_text('News Articles');
+	}
+	
+	$has = [];
+	foreach ($data['titles'] as $area => $title) {
+		$has[$area] = true;
+		switch ($area) {
+		case 'category':
+			$sql = 'SELECT category FROM categories WHERE category_id = %d';
+			$sql = sprintf($sql, wrap_category_id($title));
+			$title = wrap_db_fetch($sql, '', 'single value');
+			break;
+		case 'year':
+			break;
+		case 'month':
+			break;
+		default:
+			break;
+		}
+		$page['breadcrumbs'][] = $title;
+		if (empty($settings['hide_title'])) {
+			$page['title'][] = $title;
+		}
+	}
+	if (!empty($page['title']))
+		$page['title'] = implode(' &middot; ', $page['title']);
+
+	if (!$data['count']) {
+		foreach (array_keys($has) as $area) {
+			$data['no_news_'.$area] = true;
+		}
+		if (!$params)
+			$data['no_news_current'] = true;
+		$page['status'] = 404;
+	}
+
 	if (!empty($settings['hide_categories']))
 		foreach ($data as $id => $line)
 			unset($data[$id]['categories']);
