cc5b1330276987e6d9333523ca2e7e6f70694769
Gustaf Mossakowski <gustaf@koenige.org>
2023-08-28 18:39:38 +0000

    move wrap_page_links() to zzwrap module
    git-svn-id: file:///Users/Shared/Repos/modules/news/trunk@253 eaf79678-5e9d-4c39-9089-411efe802c85

--- news/functions.inc.php
+++ news/functions.inc.php
@@ -8,7 +8,7 @@
  * https://www.zugzwang.org/modules/news
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2022 Gustaf Mossakowski
+ * @copyright Copyright © 2022-2023 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -44,32 +44,4 @@ function mf_news_prev_next($article) {
 	$articles = wrap_db_fetch($sql, 'article_id');
 	$article += wrap_get_prevnext_flat($articles, $article['article_id'], false);
 	return $article;
-}
-
-/**
- * set $page['link'] for single item
- *
- * @param array $data (_next_identifier, _next_title, _prev_identifier, _prev_title,
- *		_main_identifier or identifier, _main_title)
- * @param string $path
- * @param string $path_overview
- * @return array
- */
-function wrap_page_links($data, $path, $path_overview) {
-	$link = [];
-	if (!empty($data['_next_identifier'])) {
-		$link['next'][0]['href'] = wrap_path($path, $data['_next_identifier']);	
-		$link['next'][0]['title'] = $data['_next_title'];
-	} else {
-		$link['next'][0]['href'] = wrap_path($path_overview, $data['_main_identifier'] ?? dirname($data['identifier']));
-		$link['next'][0]['title'] = $data['_main_title'] ?? wrap_text('Overview');
-	}
-	if (!empty($data['_prev_identifier'])) {
-		$link['prev'][0]['href'] = wrap_path($path, $data['_prev_identifier']);	
-		$link['prev'][0]['title'] = $data['_prev_title'];
-	} else {
-		$link['prev'][0]['href'] = wrap_path($path_overview, $data['_main_identifier'] ?? dirname($data['identifier']));
-		$link['prev'][0]['title'] = $data['_main_title'] ?? wrap_text('Overview');
-	}
-	return $link;
 }
