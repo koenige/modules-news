f7cd2dd0e921645660046a50004a144101d16afc
Gustaf Mossakowski <gustaf@koenige.org>
2026-07-27 16:01:59 +0200

    languages: update translations; add Type as title for type_category_id

--- zzbrick_tables/articles-categories.php
+++ zzbrick_tables/articles-categories.php
@@ -42,6 +42,7 @@ $zz['fields'][3]['add_details'] = wrap_path('default_categorytree', 'news');
 $zz['fields'][3]['show_hierarchy'] = 'main_category_id';
 $zz['fields'][3]['show_hierarchy_subtree'] = wrap_category_id('news');
 
+$zz['fields'][5]['title'] = 'Type';
 $zz['fields'][5]['field_name'] = 'type_category_id';
 $zz['fields'][5]['type'] = 'hidden';
 $zz['fields'][5]['type_detail'] = 'select';
