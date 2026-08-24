f46284d147bdb85ceac796b45b472358a8856ef4
Gustaf Mossakowski <gustaf@koenige.org>
2026-08-24 17:02:45 +0200

    remove unused function mf_news_prev_next(), is done in request/article

--- news/functions.inc.php
+++ news/functions.inc.php
@@ -14,36 +14,6 @@
 
 
 /**
- * get _prev, _next-keys for article
- *
- * @param array $article
- * @return array
- */
-function mf_news_prev_next($article) {
-	if (!empty($article['publications'])) {
-		$publication_id = reset($article['publications']);
-		$publication_id = $publication_id['category_id'];
-		$sql = 'SELECT articles.article_id, title, identifier
-			FROM articles
-			LEFT JOIN articles_categories
-				ON articles_categories.article_id = articles.article_id
-				AND articles_categories.type_category_id = /*_ID categories publications _*/
-			WHERE articles_categories.category_id = %d
-			AND published = "yes"
-			ORDER BY identifier DESC';
-		$sql = sprintf($sql, $publication_id);
-	} else {
-		$sql = 'SELECT articles.article_id, title, identifier
-			FROM articles
-			WHERE published = "yes"
-			ORDER BY identifier DESC';
-	}
-	$articles = wrap_db_fetch($sql, 'article_id');
-	$article += wrap_get_prevnext_flat($articles, $article['article_id'], false);
-	return $article;
-}
-
-/**
  * years with published articles for archive navigation
  *
  * @param int|null $current_year highlight this year in the list
